f0171f1b35d114d8684e571cdfeb8d754a6eeb58
ateight88 <doyleto@icloud.com>
2023-06-04 14:10:14 +0100

    initializr spring

---
+++ backend/src/main/java/com/tondoy/sgnnnews/SgnnNewsApplication.java
@@ -0,0 +1,13 @@
+package com.tondoy.sgnnnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgnnNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgnnNewsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tondoy/sgnnnews/SgnnNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tondoy.sgnnnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgnnNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
