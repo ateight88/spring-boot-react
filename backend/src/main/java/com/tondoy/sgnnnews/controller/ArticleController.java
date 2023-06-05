281e5e1561d161c48acaff670d0e3dd6d8d2d638
ateight88 <doyleto@icloud.com>
2023-06-05 19:13:33 +0100

    added frontend starter

--- backend/src/main/java/com/tondoy/sgnnnews/controller/ArticleController.java
+++ backend/src/main/java/com/tondoy/sgnnnews/controller/ArticleController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class ArticleController {
