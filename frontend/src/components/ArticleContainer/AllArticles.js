d7c9e1d86f78bd72412dfe7a431178e85fe35a43
ateight88 <doyleto@icloud.com>
2023-06-07 16:49:14 +0100

    updated homepage image

--- frontend/src/components/ArticleContainer/AllArticles.js
+++ frontend/src/components/ArticleContainer/AllArticles.js
@@ -49,7 +49,8 @@ const AllArticles = () => {
           <div
             className='category-background'
             style={{
-              backgroundImage: `url(https://source.unsplash.com/random/?news&sig=111)`,
+              // backgroundImage: `url(https://source.unsplash.com/random/?news&sig=111)`,
+              backgroundImage: `url(https://images.unsplash.com/photo-1618044733300-9472054094ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80)`,
             }}
           >
             <div className='all-articles-container'>
